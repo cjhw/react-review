9d3e2859d570723138eeb34269d94fc644d6db46
caijianhaoa <1784581164@qq.com>
2022-09-14 16:48:46 +0800

    实现自定义弹窗

---
+++ src/customModal/components/dialog.jsx
@@ -0,0 +1,57 @@
+import React, { useMemo, useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+
+/* 控制弹窗隐藏以及动画效果 */
+const controlShow = (f1, f2, value, timer) => {
+  f1(value)
+  return setTimeout(() => {
+    f2(value)
+  }, timer)
+}
+const RenderChildren = (props) => {
+  /* 把元素渲染到组件之外的 document.body 上  */
+  return ReactDOM.createPortal(props.children, document.body)
+}
+export default function Dialog(props) {
+  const { width, visible, closeCb, onClose, children } = props
+  /* 控制 modelShow 动画效果 */
+  const [modelShow, setModelShow] = useState(visible)
+  const [modelShowAync, setModelShowAync] = useState(visible)
+
+  useEffect(() => {
+    let timer
+    if (visible) {
+      timer = controlShow(setModelShow, setModelShowAync, visible, 30)
+    } else {
+      timer = controlShow(setModelShowAync, setModelShow, visible, 1000)
+    }
+    return function () {
+      timer && clearTimeout(timer)
+    }
+  }, [visible])
+
+  /* 执行关闭弹窗后的回调函数 closeCb */
+  useEffect(() => {
+    !modelShow && typeof closeCb === 'function' && closeCb()
+  }, [modelShow])
+
+  return (
+    <RenderChildren modelShow={modelShow} modelShowAync={modelShowAync}>
+      <div style={{ display: modelShow ? 'block' : 'none' }}>
+        <div
+          className="model_container"
+          style={{ opacity: modelShowAync ? 1 : 0 }}
+        >
+          <div className="model_wrap">
+            <div style={{ width: width + 'px' }}>{children}</div>
+          </div>
+        </div>
+        <div
+          className="model_container mast"
+          onClick={() => onClose && onClose()}
+          style={{ opacity: modelShowAync ? 0.6 : 0 }}
+        />
+      </div>
+    </RenderChildren>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Father } from './event/index'
 import Hoc from './Hoc'
+import Modal from './customModal'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Hoc></Hoc>
+    <Modal></Modal>
   </React.StrictMode>
 )
